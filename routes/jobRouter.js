c9ab8e18e038c76326a9ac9f2396c7058558a48c
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-29 12:06:46 +0530

    updated JobController

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -2,13 +2,13 @@ import { Router } from "express";
 import {
   createJob,
   deleteJob,
-  editJob,
+  updateJob,
   getAllJobs,
   getJob,
 } from "../controllers/jobController.js";
 const router = Router();
 
 router.route("/").get(getAllJobs).post(createJob);
-router.route("/:id").get(getJob).patch(editJob).delete(deleteJob);
+router.route("/:id").get(getJob).patch(updateJob).delete(deleteJob);
 
 export default router;
