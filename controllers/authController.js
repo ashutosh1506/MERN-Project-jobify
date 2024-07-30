57b9ec4ea1286b2f445a62b5339bc336f23c212b
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-30 13:18:40 +0530

    logout Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -32,3 +32,11 @@ export const login = async (req, res) => {
   });
   res.status(StatusCodes.OK).json({ msg: "Login Successful" });
 };
+
+export const logout = (req, res) => {
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).json({ msg: "User Logged Out" });
+};
