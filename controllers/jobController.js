51979427b198ff509faa8d3786321039482748ac
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-30 13:14:16 +0530

    Jobs Validation
    1. setup jobs controller, in which the user who created the job can only access it

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -2,7 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import Job from "../models/JobModel.js";
 
 export const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({});
+  const jobs = await Job.find({ createdBy: req.user.userId });
   res.status(StatusCodes.OK).json({ jobs });
 };
 
@@ -12,6 +12,7 @@ export const getJob = async (req, res) => {
 };
 
 export const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
