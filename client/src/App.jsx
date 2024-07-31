fb52564eb24d2142fd3e79ad65c49cd7f98ec4e9
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-31 18:15:29 +0530

    Reigster Page completed
    1. Axios Setup
    2. action setup
    3. Toastify setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,8 @@ import {
   AddJob,
 } from "./pages";
 
+import { action as registerAction } from "./pages/Register";
+
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
   document.body.classList.toggle("dark-theme", isDarkTheme);
@@ -36,6 +38,7 @@ const router = createBrowserRouter([
       {
         path: "register",
         element: <Register />,
+        action: registerAction,
       },
       {
         path: "login",
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,21 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Form, redirect, useNavigation, Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData(); //abstracts form data
+  const data = Object.fromEntries(formData); //Array to object
+
+  try {
+    await customFetch.post("/auth/register", data);
+    toast.success("Registration Successful");
+    return redirect("/login");
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
 
 const Register = () => {
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isSubmitting = navigation.state === "submitting";
   return (
     <Wrapper>
-      <form className="form">
+      <Form method="post" className="form">
         <Logo />
         <h4>Register</h4>
-        <FormRow type="text" name="name" />
-        <FormRow type="text" name="lastName" labelText="last name" />
-        <FormRow type="text" name="location" />
-        <FormRow type="email" name="email" />
-        <FormRow type="password" name="password" />
-        <button type="submit" className="btn btn-block">
-          submit
+        <FormRow type="text" name="name" defaultValue="john" />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="last name"
+          defaultValue="doe"
+        />
+        <FormRow type="text" name="location" defaultValue="Delhi" />
+        <FormRow type="email" name="email" defaultValue="john@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="secret1234" />
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? "submitting..." : "submit"}
         </button>
         <p>
           Already a member?
@@ -23,7 +47,7 @@ const Register = () => {
             Login
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 };
