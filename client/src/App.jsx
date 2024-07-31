6f433fa17641ef39030e4a48c09cd895f1af00b6
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-31 21:47:57 +0530

    Admin Page setup
    1. total no. of users
    2. total no. of jobs
    3. only accessible by the admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,6 +23,7 @@ import { loader as allJobsLoader } from "./pages/AllJobs";
 import { action as editJobAction } from "./pages/EditJob";
 import { loader as editJobLoader } from "./pages/EditJob";
 import { action as deleteJobAction } from "./pages/DeleteJob";
+import { loader as adminLoader } from "./pages/Admin";
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -77,6 +78,7 @@ const router = createBrowserRouter([
           {
             path: "admin",
             element: <Admin />,
+            loader: adminLoader,
           },
           {
             path: "edit-job/:id",
