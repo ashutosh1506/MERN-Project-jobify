af40e132c57110575dd3d54ee8ab8fc18fab877f
ashutosh1506 <guptaashutosh502@gmail.com>
2024-08-01 21:54:36 +0530

    Stats Page Completed
    1. Group by job status
    2. Chart Containers
    2.1 Area chart and Bar chart

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -5,6 +5,7 @@ import {
   updateJob,
   getAllJobs,
   getJob,
+  showStats,
 } from "../controllers/jobController.js";
 import {
   validateIdParam,
@@ -17,6 +18,9 @@ router
   .route("/")
   .get(getAllJobs)
   .post(checkForTestUser, validateJobInput, createJob);
+
+router.route("/stats").get(showStats);
+
 router
   .route("/:id")
   .get(validateIdParam, getJob)
