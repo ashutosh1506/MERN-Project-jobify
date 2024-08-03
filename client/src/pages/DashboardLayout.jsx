bf1be5e93fecbd4f09ef373ed20efa05b9538e55
ashutosh1506 <guptaashutosh502@gmail.com>
2024-08-03 19:59:23 +0530

    React Query and other extra features
    1. Error Element
    2. upload image fix
    3. Global loading
    4. React Queries
    5. Axios Interceptors
    6. Security Packages

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import {
   Outlet,
   redirect,
@@ -11,11 +11,19 @@ import { BigSidebar, Loading, Navbar, SmallSidebar } from "../components";
 import { checkDefaultTheme } from "../App";
 import customFetch from "../utils/customFetch";
 import { toast } from "react-toastify";
+import { useQuery } from "@tanstack/react-query";
 
-export const loader = async () => {
-  try {
+const userQuery = {
+  queryKey: ["user"],
+  queryFn: async () => {
     const { data } = await customFetch.get("/users/current-user");
     return data;
+  },
+};
+
+export const loader = (queryClient) => async () => {
+  try {
+    return await queryClient.ensureQueryData(userQuery);
   } catch (error) {
     return redirect("/");
   }
@@ -23,13 +31,14 @@ export const loader = async () => {
 
 const DashboardContext = createContext();
 
-const DashboardLayout = () => {
-  const { user } = useLoaderData();
+const DashboardLayout = ({ queryClient }) => {
+  const { user } = useQuery(userQuery).data;
   const navigate = useNavigate();
   const navigation = useNavigation();
   const isPageLoading = navigation.state === "loading";
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme());
+  const [isAuthError, setIsAuthError] = useState(false);
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
@@ -45,9 +54,29 @@ const DashboardLayout = () => {
   const logoutUser = async () => {
     navigate("/");
     await customFetch.get("/auth/logout");
+    queryClient.invalidateQueries();
+
     toast.success("Logout successful");
   };
 
+  customFetch.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error?.response?.status === 401) {
+        setIsAuthError(true);
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  useEffect(() => {
+    if (!isAuthError) {
+      return;
+    }
+    logoutUser();
+  }, [isAuthError]);
   return (
     <DashboardContext.Provider
       value={{
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,9 +5,16 @@ import {
   validateRegisterInput,
 } from "../middleware/validationMiddleware.js";
 const router = Router();
+import rateLimiter from "express-rate-limit";
 
-router.post("/login", validateLoginInput, login);
-router.post("/register", validateRegisterInput, register);
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 15,
+  message: { msg: "IP rate limit exceeded, Try after 15 minutes." },
+});
+
+router.post("/login", apiLimiter, validateLoginInput, login);
+router.post("/register", apiLimiter, validateRegisterInput, register);
 router.get("/logout", logout);
 
 export default router;
